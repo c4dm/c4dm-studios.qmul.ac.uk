6a0e340b225fa450c1d13299ed0551d424863a84
ciarancorr <anonycron@outlook.com>
2026-09-08 10:57:20 +0100

    Updates

--- assets/index-B68_Ux5c.js
+++ assets/index-B68_Ux5c.js
@@ -71,6 +71,8 @@ We also offer our facilities for hire to external clients, such as recording art
 
 [View Complete Inventory (requires Queen Mary login)](https://qmulprod.sharepoint.com/sites/Studio/Lists/MGMT%20%20Inventory/Inventory.aspx?env=WebViewList)
 
+[View Equipment Manuals (requires Queen Mary login)](https://qmulprod.sharepoint.com/:f:/s/Studio/IgDgxFG4ey_nRa5pmWsxK7vEAYycRlsFKYop0xw11iuaCbQ?e=iPwNK4)
+
 ## Microphones
 
 <table><tbody>
